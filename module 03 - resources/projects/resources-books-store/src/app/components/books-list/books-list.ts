2bc3582eb0af1c00e11f22ddc6b6b21fc56445d4
Kobi Hari <kobi2294@yahoo.com>
2025-11-03 14:43:07 +0200

    resources

---
+++ module 03 - resources/projects/resources-books-store/src/app/components/books-list/books-list.ts
@@ -0,0 +1,17 @@
+import { Component, inject, input, model, signal } from '@angular/core';
+import { Book } from '../../models/book';
+import { Busy } from "../busy/busy";
+import { StateService } from '../../services/state-service';
+
+@Component({
+  selector: 'app-books-list',
+  imports: [Busy],
+  templateUrl: './books-list.html',
+  styleUrl: './books-list.scss',
+})
+export class BooksList {
+  readonly selectedBookId = signal<string>('27eec969-1138-43c3-9e11-fe92cda1e892');
+
+  readonly state = inject(StateService);
+
+}
---
+++ module 03 - resources/projects/resources-books-store/src/app/services/state-service.ts
@@ -0,0 +1,46 @@
+import { Inject, Injectable, resource, signal } from "@angular/core";
+import { Book } from "../models/book";
+import { httpResource } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StateService {
+    readonly apiBase = 'http://localhost:3000/api/books';
+    readonly wsBase = 'ws://localhost:3000/ws';
+
+    #keyword = signal('the');
+
+    // #searchResult = resource({
+    //     params: () => ({ keyword: this.#keyword() }),
+    //     loader: options => this.#searchKeywordPromise(options.params.keyword, options.abortSignal), 
+    //     defaultValue: []
+    // });
+
+    #searchResult = httpResource<Book[]>(() => ({
+        url: `${this.apiBase}/search`,
+        params: { q: this.#keyword() } 
+    }), {
+        defaultValue: []
+    });
+
+    get keyword() {
+        return this.#keyword.asReadonly();
+    }
+
+    get searchResult() {
+        return this.#searchResult.asReadonly();
+    }
+
+    setKeyword(value: string) {
+        console.log('Setting keyword to', value);
+        this.#keyword.set(value);
+    }
+
+    constructor() {}
+
+    #searchKeywordPromise(value: string, abortSignal?: AbortSignal): Promise<Book[]> {
+        return fetch(`${this.apiBase}/search?q=${value}`, { signal: abortSignal})
+            .then(resp => resp.json());
+    }
+}
