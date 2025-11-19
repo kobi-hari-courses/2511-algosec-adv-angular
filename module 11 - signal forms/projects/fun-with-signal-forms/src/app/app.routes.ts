e5ef3ba6b07121c6fed148eca8ef17de984764ce
Kobi Hari <kobi2294@yahoo.com>
2025-11-19 10:44:19 +0200

    some styling

--- module 11 - signal forms/projects/fun-with-signal-forms/src/app/app.routes.ts
+++ module 11 - signal forms/projects/fun-with-signal-forms/src/app/app.routes.ts
@@ -4,4 +4,5 @@ export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'd1-simple-form' },
     { path: 'd1-simple-form', loadComponent: () => import('./pages/d1-simple-form/d1-simple-form') },
     { path: 'd2-simple-validations', loadComponent: () => import('./pages/d2-simple-validations/d2-simple-validations') },
+    { path: 'd3-advanced-validations', loadComponent: () => import('./pages/d3-advanced-validations/d3-advanced-validations') },
 ];
---
+++ module 11 - signal forms/projects/fun-with-signal-forms/src/app/pages/d3-advanced-validations/d3-advanced-validations.ts
@@ -0,0 +1,30 @@
+import { Component, signal } from '@angular/core';
+import { DinnerReview } from '../../model/dinner-review.model';
+import { form, max, maxLength, min, minLength, required } from '@angular/forms/signals';
+import { SharedModule } from '../../shared.module';
+
+@Component({
+  selector: 'app-d3-advanced-validations',
+  imports: [SharedModule],
+  templateUrl: './d3-advanced-validations.html',
+  styleUrl: './d3-advanced-validations.scss',
+})
+export default class D3AdvancedValidations {
+  readonly model = signal<DinnerReview>({
+    username: '', 
+    food: '', 
+    rating: 5, 
+    comeBack: true
+  });
+
+  readonly dinnerForm = form(this.model, x => {
+    required(x.username);
+    minLength(x.username, 5);
+    maxLength(x.username, 15);
+    required(x.food);
+    required(x.rating);
+    max(x.rating, 10);
+    min(x.rating, 1);
+  });
+
+}
